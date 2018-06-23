d8a7bdf7e4ae759142760fe4e63b2ece6ad314a8
Eppleton IT <seal@dukescript.com>
2018-06-23 05:02:29 +0200

    Checking proper file headers

--- testing/app/src/main/java/com/dukescript/api/device/tests/DeviceInfoCheck.java
+++ testing/app/src/main/java/com/dukescript/api/device/tests/DeviceInfoCheck.java
@@ -1,25 +1,21 @@
 package com.dukescript.api.device.tests;
 
-import com.dukescript.api.mobile.device.Device;
-import com.dukescript.api.mobile.device.DeviceServices;
-import static org.junit.Assert.assertNotNull;
-import org.junit.Test;
-
 /*
- * The MIT License
- *
- * Copyright 2018 devel.
- *
+ * #%L
+ * Testing App - Device API - a library from the "DukeScript" project.
+ * %%
+ * Copyright (C) 2015 - 2018 Dukehoff GmbH
+ * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,7 +23,13 @@ import org.junit.Test;
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ * #L%
  */
+
+import com.dukescript.api.mobile.device.Device;
+import com.dukescript.api.mobile.device.DeviceServices;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 
 public class DeviceInfoCheck {
     @Test
